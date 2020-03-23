a6c953377c1cc8d5dbb2a117c832b9ffb2652ca3
Trix <trxior@gmail.com>
2020-03-23 14:21:19 +0100

    add jQuery and owl-carousel plugin

--- js/main.js
+++ js/main.js
@@ -8,3 +8,32 @@ menuBtn.addEventListener('click', () => {
         nav.style = 'display: none';
     }
 });
+
+$(document).ready(function () {
+    $(".gallery__owl-carousel").owlCarousel({
+        navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+        nav: true,
+        dots: false,
+        loop: true,
+        margin: 50,
+        responsive: {
+            0: {
+                margin: 25,
+                items: 1
+            },
+            768: {
+                items: 2
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $(".timestamp__owl-carousel").owlCarousel({
+        nav: false,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        items: 1
+    })
+});
